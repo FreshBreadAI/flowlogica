d35e9726349a2b617f23443fedbc117b5587ccd4
Ody22 <208146711+FreshBreadAI@users.noreply.github.com>
2025-06-19 11:10:16 +0100

    updated home config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@
 const nextConfig = {
   output: "export",
   images: { unoptimized: true },
+  eslint: {
+    ignoreDuringBuilds: true, // This is the magic line you add
+  },
 };
 
 module.exports = nextConfig;
